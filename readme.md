6de7068f0697566a2e057f2c9ec6ad2cc56863ec
Gxs16 <847486005@qq.com>
2023-06-27 22:35:29 +0800

    refactor

---
+++ readme.md
@@ -0,0 +1,5 @@
+VRPTW-Branch-and-Price
+
+* branch and bound
+* column generation
+* espprc-labels-setting algo
--- src/algo/Main.java
+++ src/algo/Main.java
@@ -5,23 +5,23 @@ import java.util.ArrayList;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		branchandbound bp = new branchandbound();
-		paramsVRP instance = new paramsVRP();
-		instance.initParams("dataset/C101.TXT");
-		ArrayList<route> initRoutes = new ArrayList<route>();
-		ArrayList<route> bestRoutes = new ArrayList<route>();
+    public static void main(String[] args) throws IOException {
+        BranchAndBound bp = new BranchAndBound();
+        ParamsVRP instance = new ParamsVRP();
+        instance.initParams("dataset/R211.TXT");
+        ArrayList<Route> initRoutes = new ArrayList<>();
+        ArrayList<Route> bestRoutes = new ArrayList<>();
 
-		bp.BBNode(instance, initRoutes, null, bestRoutes, 0);
-		double optCost = 0;
-		System.out.println();
-		System.out.println("solution >>>");
-		for (route bestRoute : bestRoutes) {
-			System.out.println(bestRoute.path);
-			optCost += bestRoute.cost;
-		}
+        bp.node(instance, initRoutes, null, bestRoutes, 0);
+        double optCost = 0;
+        System.out.println();
+        System.out.println("solution >>>");
+        for (Route bestRoute : bestRoutes) {
+            System.out.println(bestRoute.path);
+            optCost += bestRoute.cost;
+        }
 
-		System.out.println("\nbest Cost = " + optCost);
-	}
+        System.out.println("\nbest Cost = "+optCost);
+    }
 
 }
